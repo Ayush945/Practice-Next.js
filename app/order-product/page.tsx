d6db041b8d2779f6a95bd3789a33ea3ed2f17045
Ayush <ayushthapa0010@gmail.com>
2024-06-30 10:01:11 +0545

    automatic navigation

---
+++ app/order-product/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { useRouter } from "next/navigation";
+export default function page() {
+    const router=useRouter();
+
+    const handleClick=()=>{
+        console.log("Placing Order...");
+        //router.replace("/");//removes browser history stack
+        //router.push("/");//add a new to browser history stack
+        //router.back();//navigates to previous page on history stack
+        router.forward();
+    }
+    return (
+    <>
+    <h1>Order Product</h1>
+    <button onClick={handleClick}>Place Order</button>
+    </>
+  )
+}
